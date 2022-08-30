fdb82bfb2b9d942225dca541c3cba864eaea6341
Allen <madriagachrisllen21@gmail.com>
2022-08-30 11:41:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from "react";
 
+import {useEffect} from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+// b90ab811
+
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=b90ab811';
+ 
 const App = () => {
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+}
+
+useEffect(() => {
+    searchMovies('Spiderman');
+}, []);
+
     return(
         <h1>App</h1>
     )
